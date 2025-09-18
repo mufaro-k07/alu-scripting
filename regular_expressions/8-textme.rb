8767362ba2c0bbd65ebabf7410f051d444d45d34
mufaro-k07 <m.kunze@alustudent.com>
2025-09-18 14:50:58 -0700

    Textme

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -3,8 +3,8 @@ log = ARGV[0]
 
 matches = log.scan(/from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
 
-# matches is an arrays of arrays; we just need to find the first match
+# matches is an array of arrays; we just need to find the first match
 if matches.any?
-  sender, reciever, flags = matches[0]
+  sender, receiver, flags = matches[0]
   puts "#{sender},#{receiver},#{flags}"
 end
