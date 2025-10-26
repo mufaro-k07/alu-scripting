fe8461e9dccf260cfa797b72c5e6a880f1ff3e72
mufaro-k07 <m.kunze@alustudent.com>
2025-10-26 19:22:34 +0000

    Outputcheck

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prints titles of the first 10 hot posts for a subreddit.
+1-top_ten: Print titles of the first 10 hot posts for a subreddit.
 """
 import requests
 
@@ -10,25 +10,43 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts for `subreddit`.
     If the subreddit is invalid or an error occurs, prints None.
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
+    if not subreddit or not isinstance(subreddit, str):
+        print(None)
         return
 
     headers = {
-        "User-Agent": "alu.api_advanced:0.1 (by mufaro-k07)"
+        "User-Agent": "linux:alu.api_advanced:0.1 (by mufaro-k07)",
+        "Accept": "application/json",
     }
     params = {"limit": 10}
 
     url1 = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
-    response = get(url, headers=headers, params=params, allow_redirects=False)
-    results = response.json()
+    url2 = "https://api.reddit.com/r/{}/hot".format(subreddit)
 
     try:
-	my_data = results.get('data').get('children')
+        r = requests.get(url1, headers=headers, params=params,
+                         allow_redirects=False, timeout=10)
+        if r.status_code == 200:
+            data = r.json()
+        else:
+            r2 = requests.get(url2, headers=headers, params=params,
+                              allow_redirects=False, timeout=10)
+            if r2.status_code != 200:
+                print(None)
+                return
+            data = r2.json()
+    except (requests.RequestException, ValueError):
+        print(None)
+        return
 
-	for i in my_data:
-	    print(i.get('data').get('title'))
+    posts = data.get("data", {}).get("children", [])
+    if not posts:
+        print(None)
+        return
 
-    except Exception:
-	print("None")
+    for post in posts[:10]:
+        title = post.get("data", {}).get("title")
+        if title is None:
+            print(None)
+            return
+        print(title)
